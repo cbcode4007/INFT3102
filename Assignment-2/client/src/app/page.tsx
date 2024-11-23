ebdc3806a704687fcd022267a91154bd4998a087
Colin Bond <colin.bond@dcmail.ca>
2024-11-22 21:29:36 -0500

    Assignment 2 Final Commit

--- Assignment-2/client/src/app/page.tsx
+++ Assignment-2/client/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h3>Welcome!</h3>
       <p>This is my site for Assignment 2 of Web Development Frameworks. It iterates on its predecessor with the use of Next.js as an SSG and Strapi to retrieve its content from a CMS.</p>
+      <img src="http://localhost:1337/uploads/nextjs_and_strapi_56f3814afb.jpg" className="center-img"></img>
     </div>
   );
 }
